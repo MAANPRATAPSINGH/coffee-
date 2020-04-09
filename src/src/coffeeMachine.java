c58111aa2c777c938373b1c5e9059b32534a946c
MAANPRATAPSINGH <59688831+MAANPRATAPSINGH@users.noreply.github.com>
2020-04-10 01:37:21 +0530

    this is the default amount of water ,milk ,beans ,cups and money in the machine these are static now we don't need to make an object

--- src/src/coffeeMachine.java
+++ src/src/coffeeMachine.java
@@ -2,6 +2,11 @@
 import java.util.Scanner;
 
 public class coffeeMachine {
+    static int WATER = 400;
+    static int MILK = 540;
+    static int BEANS = 120;
+    static int CUPS = 9;
+    static int money = 550;
 
 }
 
