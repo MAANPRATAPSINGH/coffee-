1a9acfec11849b9c2fda214aa5b60e401fcbf942
MAANPRATAPSINGH <59688831+MAANPRATAPSINGH@users.noreply.github.com>
2020-04-10 01:46:02 +0530

    here we use switch cases inside a do while loop in this we will perform the function according to the input of the user .user give the inputs like (buy ,take ,fill etc.)here we only calling the methods we will define them soon

--- src/src/coffeeMachine.java
+++ src/src/coffeeMachine.java
@@ -17,6 +17,37 @@ public class coffeeMachine {
     static Status curStatus = Status.CHOOSING;
     public static void main(String[] args) {
 
+        do {
+            System.out.println(curStatus);
+            System.out.print("Write action (buy, fill, take, remaining, exit): ");
+            String action = scanner.next();
+            switch (action) {
+                case "buy":
+                    curStatus = Status.BUYING;
+                    System.out.println(curStatus);
+                    buy();
+                    break;
+                case "fill":
+                    curStatus = Status.FILLING;
+                    System.out.println(curStatus);
+                    fill();
+                    break;
+                case "take":
+                    curStatus = Status.TAKING;
+                    System.out.println(curStatus);
+                    take();
+                    break;
+                case "remaining":
+                    curStatus = Status.REMAINING;
+                    System.out.println(curStatus);
+                    remaining();
+                    break;
+                case "exit":
+                    exit();
+                    System.out.println(curStatus);
+                    break;
+            }
+        } while (curStatus != Status.EXITING);
     }
 
 
