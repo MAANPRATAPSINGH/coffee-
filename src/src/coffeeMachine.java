36311205eb97311655644f80f2d5a66718820186
MAANPRATAPSINGH <59688831+MAANPRATAPSINGH@users.noreply.github.com>
2020-04-10 01:38:56 +0530

    in final stage we made our code by the view of oop concepts by defining different methods of function

--- src/src/coffeeMachine.java
+++ src/src/coffeeMachine.java
@@ -7,6 +7,13 @@ public class coffeeMachine {
     static int BEANS = 120;
     static int CUPS = 9;
     static int money = 550;
+    static Boolean exit = false;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    enum Status {
+        CHOOSING, BUYING, FILLING, TAKING, REMAINING, EXITING
+    }
 
 }
 
